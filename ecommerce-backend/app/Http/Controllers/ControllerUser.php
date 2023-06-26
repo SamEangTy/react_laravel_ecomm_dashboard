0ac60734f3971cd17825b3ea11ab3665dc66deb0
SamEangTy <121166611+SamEangTy@users.noreply.github.com>
2023-06-26 21:29:51 +0700

    header

---
+++ ecommerce-backend/app/Http/Controllers/ControllerUser.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ControllerUser extends Controller
+{
+    function register(){
+        return "register";
+    }
+}
