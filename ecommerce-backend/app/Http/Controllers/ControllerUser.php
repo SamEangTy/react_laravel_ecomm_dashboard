8addfe0d78db1b116a8dcebad8099fde656ff800
SamEangTy <121166611+SamEangTy@users.noreply.github.com>
2023-06-26 22:24:18 +0700

    header front and register back

--- ecommerce-backend/app/Http/Controllers/ControllerUser.php
+++ ecommerce-backend/app/Http/Controllers/ControllerUser.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class ControllerUser extends Controller
 {
-    function register(){
-        return "register";
+    function register(Request $req){
+        $user = new User;
+        $user->name = $req->input('name');
+        $user->email = $req->input('email');
+        $user->password = Hash::make($req->input('password'));
+        $user->save();
+        return $user;
     }
 }
